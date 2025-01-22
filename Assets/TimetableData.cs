339d13c2b4052c2e8649a628a624cd7fdbd87e09
DESKTOP-V0CO5AL\13204 <1320494699@qq.com>
2025-01-22 16:34:42 +0800

    添加粗略的表格样式

---
+++ Assets/TimetableData.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TimetableData
+{
+    
+}
+[System.Serializable]
+public class TimetableItemData
+{
+    //学生姓名
+    public string studentName;
+    //地点
+    public string location;
+    //时间
+    public string time;
+    //星期几
+    public string weekday;
+    //第几节课
+    public string classNumber;
+}
